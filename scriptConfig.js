1d4df82cd8918a61ba149d31932e392712945d3f
Mulsd <Mulsd@qq.com>
2023-06-10 18:21:47 +0800

    工作环境初始化

--- scriptConfig.js
+++ scriptConfig.js
@@ -3,9 +3,9 @@
 var projects = [ //项目数组，放的是每一个要编译的项目，
   {
     id: 1, //项目号
-    compile: false, //是否编译
-    name: "云控demo", //项目名称，需要和文件夹名相同
-    main: "./test.js" //主入文件，入口文件，按autojs目录，一般和project.json 同级
+    compile: true, //是否编译
+    name: "Auto-js", //项目名称，需要和文件夹名相同
+    main: "./reday.js" //主入文件，入口文件，按autojs目录，一般和project.json 同级
   },
   {
     id: 2, compile: false, name: "dy", main: "./main.js",
