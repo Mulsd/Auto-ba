f63f4540fecf3fb6534cffd37e1b6ff96d410630
Mulsd <Mulsd@qq.com>
2023-06-18 22:06:51 +0800

    尝试远程更新

--- work/Auto-ba/update.js
+++ work/Auto-ba/update.js
@@ -1,7 +1,7 @@
-var url = "https://github.com/Mulsd/Auto-ba/dist/";
+var url1 = "https://fastly.jsdelivr.net/gh/Mulsd/Auto-ba@master/dist/Auto-ba/ready.js";
 var res = http.get(url);
 if(res.statusCode != 200){
     toast("更新失败!");
 }
-files.writeBytes("/sdcard/Autoba/", res.body.bytes());
+files.writeBytes("/sdcard/Auto-ba/ready.js", res.body.bytes());
 toast("更新成功")
