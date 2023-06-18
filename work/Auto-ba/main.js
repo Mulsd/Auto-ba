3d654cbc99b3ca3b01dcf430a8db2948ba00bb74
Mulsd <Mulsd@qq.com>
2023-06-18 12:28:10 +0800

    试错

--- work/Auto-ba/main.js
+++ work/Auto-ba/main.js
@@ -5,27 +5,5 @@ ui.layout(
     </frame>
 );
 ui.start.on("click",()=>{
-    launch("com.nexon.bluearchive");
-    setScreenMetrics(1080, 1920);
-    click(1746, 857);
-    sleep(2000);
-    click(1540, 756);
-    sleep(2000);
-    click(571, 698);
-    sleep(2000);
-    click(690, 1014);
-    sleep(2000);
-    click(571, 577);
-    sleep(2000);
-    click(690, 1014);
-    sleep(2000);
-    click(1218, 897);
-    sleep(2000);
-    click(970, 854);
-    sleep(2000);
-    click(1753, 999);
-    sleep(2000);
-    click(961, 693);
-    sleep(2000);
-    back();
+    engines.execScript("open", Source); 
 }); 
